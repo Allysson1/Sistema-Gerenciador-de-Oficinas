1e593ee64590845e4fc662f28e941b2759580b76
Állysson Ernandes <allyssonoliveira12345@gmail.com>
2023-07-17 21:05:32 -0300

    fix
    correção de erros

--- V_cadastraUsuario.php
+++ V_cadastraUsuario.php
@@ -11,6 +11,7 @@
 </head>
 
 <?php include 'header.php'; ?>
+<?php include 'message.php'; ?>
 
 <body class="container-fluid row justify-content-center">
 
@@ -21,34 +22,34 @@
 
 <h4 class="col-12 m-3 pb-4 border-bottom">Cadastro de Funcionários</h4>
 
-<form action="usuario.php" method="post" class="ml-3 ml-md-3 ml-lg-3 col-12 col-lg-12">
+<form action="Code_Usuario.php" method="post" class="ml-3 ml-md-3 ml-lg-3 col-12 col-lg-12">
    
 
     <div class=" col-12 col-md-6 float-md-left">
         <label class="col-12 col-md-12 mt-5 mt-md-5" 
     for="email">Nome do Funcionário:</label>
     <input class="col-12 col-md-12  mt-md-1 p-3 textBox" 
-    type="text" placeholder="Digite seu nome completo:" autofocus="true"/>
+    type="text" name="nomeFuncionario" placeholder="Digite seu nome completo:" autofocus="true"/>
     </div>
     
     <div class=" col-12 col-md-6 float-md-left">
         <label class="col-12 col-md-12  mt-5 mt-md-5" 
     for="password">Usuário:</label>
     <input class="col-12 col-md-12 mt-md-1 p-3 textBox" 
-    type="text" placeholder="Digite seu nome de Usuário:"/>
+    type="text" name="usuario" placeholder="Digite seu nome de Usuário:"/>
     </div>
 
 
     <div class=" col-12 col-md-12 float-md-left">
         <label class="col-12 col-md-12 mt-5 mt-md-5" 
     for="password">Senha:</label>
-    <input class="col-12 col-md-6 mt-md-1  p-3 textBox" 
-    type="password" placeholder="Digite sua Senha:"/>
+    <input class="col-12 col-md-6 mt-md-1 p-3 textBox" 
+    type="password" name="senha" placeholder="Digite sua Senha:"/>
     </div>
 
 
     <div class="float-right col-5 col-md-3 mt-5 mt-md-5">
-    <button class="col-12 ml-1 p-3 btn btn-primary" type="submit">Cadastrar</button>      
+    <button class="col-12 ml-1 p-3 btn btn-primary" type="submit" name="save_funcionario">Cadastrar</button>      
     </div>
 
 </form>
