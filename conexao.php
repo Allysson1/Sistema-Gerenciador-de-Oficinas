ca9298ea5060db831589cca88a57be9f95369890
Állysson Ernandes <allyssonoliveira12345@gmail.com>
2023-07-26 20:38:45 -0300

    fix
    correção de código

--- conexao.php
+++ conexao.php
@@ -1,10 +1,25 @@
 <?php
 
-$con = new mysqli('localhost', 'root', '', 'bd_tg');
+// $con = new PDO('localhost', 'root', '', 'bd_tg');
 
+
+// if(!$con){
+//     die("Não foi possivel conectar ao MySQL " . mysqli_connect_error());
+// }
+
+
+
+
+/* Connect to a MySQL database using driver invocation */
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'bd_tg';
+
+$con = mysqli_connect($host, $user, $password, $database);
 
 if(!$con){
-    die("Não foi possivel conectar ao MySQL " . mysqli_connect_error());
+    die('Connection Failed'. mysqli_connect_error());
 }
 
 ?>
