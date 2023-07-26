2b3b353969e0ef6098ad2133b176efef0c447b00
Állysson Ernandes <allyssonoliveira12345@gmail.com>
2023-07-26 20:41:29 -0300

    fix
    correção de código

--- V_cadastraUsuario.php
+++ V_cadastraUsuario.php
@@ -1,61 +1,69 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../TGFATEC/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../TGFATEC/css/style.css"> 
-    <title>Cadastro de Funcionários - SGO</title>
-    <link rel="shortcut icon" type="imagex/png" href="../TGFATEC/images/icon.volante.svg">
-</head>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../TGFATEC/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../TGFATEC/css/style.css"> 
+        <title>Cadastro de Funcionários - SGO</title>
+        <link rel="shortcut icon" type="imagex/png" href="../TGFATEC/images/icon.volante.svg">
+    </head>
 
-<?php include 'header.php'; ?>
-<?php include 'message.php'; ?>
-
-<body class="container-fluid row justify-content-center">
+    <!-- <?php include 'header.php'; ?> -->
 
 
-
-     <!-- formulário de login -->
-     <main class="mt-5 col-12 col-sm-12 col-md-12 col-lg-10"> 
-
-<h4 class="col-12 m-3 pb-4 border-bottom">Cadastro de Funcionários</h4>
-
-<form action="Code_Usuario.php" method="post" class="ml-3 ml-md-3 ml-lg-3 col-12 col-lg-12">
-   
-
-    <div class=" col-12 col-md-6 float-md-left">
-        <label class="col-12 col-md-12 mt-5 mt-md-5" 
-    for="email">Nome do Funcionário:</label>
-    <input class="col-12 col-md-12  mt-md-1 p-3 textBox" 
-    type="text" name="nomeFuncionario" placeholder="Digite seu nome completo:" autofocus="true"/>
-    </div>
-    
-    <div class=" col-12 col-md-6 float-md-left">
-        <label class="col-12 col-md-12  mt-5 mt-md-5" 
-    for="password">Usuário:</label>
-    <input class="col-12 col-md-12 mt-md-1 p-3 textBox" 
-    type="text" name="usuario" placeholder="Digite seu nome de Usuário:"/>
-    </div>
+    <body class="container-fluid row justify-content-center">
 
 
-    <div class=" col-12 col-md-12 float-md-left">
-        <label class="col-12 col-md-12 mt-5 mt-md-5" 
-    for="password">Senha:</label>
-    <input class="col-12 col-md-6 mt-md-1 p-3 textBox" 
-    type="password" name="senha" placeholder="Digite sua Senha:"/>
-    </div>
+
+        <!-- formulário de login -->
+        <main class="mt-5 col-12 col-sm-12 col-md-12 col-lg-10"> 
+
+            <?php include ('message.php'); ?>
+
+            <h4 class="col-12 m-3 pb-4 border-bottom">Cadastro de Funcionários</h4>
+
+            <form action="Code_Usuario.php" method="POST" class="ml-3 ml-md-3 ml-lg-3 col-12 col-lg-12">
+            
+
+                <div class=" col-12 col-md-6 float-md-left">
+                    <label class="col-12 col-md-12 mt-5 mt-md-5" 
+                for="email">Nome do Funcionário:</label>
+                <input class="col-12 col-md-12  mt-md-1 p-3 textBox" 
+                type="text" name="nomeFuncionario" placeholder="Digite seu nome completo:" autofocus="true"/>
+                </div>
+                
+                <div class=" col-12 col-md-6 float-md-left">
+                    <label class="col-12 col-md-12  mt-5 mt-md-5" 
+                for="password">Usuário:</label>
+                <input class="col-12 col-md-12 mt-md-1 p-3 textBox" 
+                type="text" name="usuario" placeholder="Digite seu nome de Usuário:"/>
+                </div>
 
 
-    <div class="float-right col-5 col-md-3 mt-5 mt-md-5">
-    <button class="col-12 ml-1 p-3 btn btn-primary" type="submit" name="save_funcionario">Cadastrar</button>      
-    </div>
+                <div class=" col-12 col-md-12 float-md-left">
+                    <label class="col-12 col-md-12 mt-5 mt-md-5" 
+                for="password">Senha:</label>
+                <input class="col-12 col-md-6 mt-md-1 p-3 textBox" 
+                type="password" name="senha" placeholder="Digite sua Senha:"/>
+                </div>
 
-</form>
 
-</main>
+                <div class="float-right col-5 col-md-3 mt-5 mt-md-5">
+                <button class="col-12 ml-1 p-3 btn btn-primary" type="submit" name="save_funcionario">Cadastrar</button>      
+                </div>
 
-    
-</body>
+            </form>
+
+        </main>
+
+        <script src="../TGFATEC/js/bootstrap.bundle.min.js"></script>
+        <script src="../TGFATEC/js/bootstrap.min.js"></script>
+        
+    </body>
 </html>
