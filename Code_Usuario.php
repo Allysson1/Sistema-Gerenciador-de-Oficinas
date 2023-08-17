d1e80b107598b46fecb22b88842702cb9c0c0319
Állysson Ernandes <allyssonoliveira12345@gmail.com>
2023-08-16 21:32:37 -0300

    feat
    adiciona funções de update e delete para o funcionário

--- Code_Usuario.php
+++ Code_Usuario.php
@@ -97,6 +97,50 @@ if (isset($_POST['save_funcionario'])){
     }
     
 }
+
+
+if (isset($_POST['delete_funcionario'])){
+
+    $funcionario_id = mysli_real_escape_string($con, $_POST['delete_funcionario']);
+
+    $query = "UPDATE usuario SET status = 'D' WHERE idUsuario = '$funcionario_id'";
+    $query_run = mysqli_query($con, $query);
+
+    if ($query_run){
+        $_SESSION['message'] = "funcionário excluído com sucesso.";
+        header("location: V_VisualizaUsuarios.php");
+        exit(0);
+    }
+    else{
+        $_SESSION['message'] = "Não foi possivel excluir o funcionário";
+        header("location: V_VisualizaUsuarios.php");
+        exit(0);
+    }
+}
+
+
+if (isset($_POST['update_funcionario'])){
+
+    $funcionario_id = mysli_real_escape_string($con, $_POST['funcionario_id']);
+
+    $nome = mysqli_real_escape_string($con, $_POST['nomeFuncionario']);
+    $usuario = mysqli_real_escape_string($con, $_POST['usuario']);
+    $senha = mysqli_real_escape_string($con, $_POST['senha']);
+
+    $query = "UPDATE usuario SET Nome='$nome', Usuario = '$usuario', Senha = '$senha'
+     WHERE idUsuario = '$funcionario_id' ";
+
+     if($query_run){
+        $_SESSION['message'] = 'Funcionário atualizado com sucesso';
+        header("location: V_VisualizaUsuarios.php")
+        exit(0);
+     }
+     else{
+        $_SESSION['message'] = "Não foi possivel atualizar o funcionário";
+        header("location: V_VisualizaUsuarios.php");
+        exit(0);
+    }
+}
    
     
 
