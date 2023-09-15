f12dcbbc0671a313bfe3761364075c11dec9230d
Állysson Ernandes <allyssonoliveira12345@gmail.com>
2023-09-14 22:25:32 -0300

    fix
    correção de erros

--- V_redefineSenha.php
+++ V_redefineSenha.php
@@ -13,7 +13,7 @@
 <!-- utiliza a mesma classe da tela de login para evitar escrever código -->
 <body class="TelaLogin container-fluid row justify-content-center">
 
-    <div class="row mt-5 pt-md-5 pt-lg-0">
+    <div class="row mt-3 pt-md-5 pt-lg-0">
     <img class="pt-4 pl-4" src="../TGFatec/images/icon.volante.svg" alt="imagem de um volande de carro simples"> 
     <p class="TICar pt-5 pl-4">TI Car</p>
     </div>
@@ -31,7 +31,7 @@
             <input class="col-12 p-3 textBox" name="usuario" type="text" placeholder="Digite seu usuário:" autofocus="true"/>
             <label class="col-12 mt-5" for="password">Nova Senha Senha:</label>
             <input class="col-12 p-3 textBox" name="senha" type="password" placeholder="Digite sua senha:"/>
-            <label class="col-12 mt-5" for="password">Confirme a Nova Senha Senha:</label>
+            <label class="col-12 mt-5" for="password">Confirme a Nova Senha:</label>
             <input class="col-12 p-3 textBox" name="confSenha" type="password" placeholder="Digite novamente sua senha:"/>
 
             <button class="col-6 m-5 p-3 btn btn-primary" type="submit" name="Redefinir_Senha">Confirmar</button>
