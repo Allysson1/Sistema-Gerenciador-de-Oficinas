df7f67adfa698a8cb5cdb7c08e7592166774c51d
AnaFurtonato <anafortunato44@gmail.com>
2023-09-24 20:05:57 -0300

    Modal

--- ordemServico.php
+++ ordemServico.php
@@ -99,7 +99,7 @@
     </main>
     
     <!-- Modal de Alteração -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="modalAlterarOrdemServico" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
