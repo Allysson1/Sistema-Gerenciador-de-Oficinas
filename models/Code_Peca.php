9d817eee9e3e8a8768f7a837bcbb5ec2d829a024
JulioG00 <jukagabriel00@gmail.com>
2023-10-23 19:32:38 -0300

    cadastro de peças

---
+++ models/Code_Peca.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require '../utils/conexao.php';
+
+if (isset($_POST['SavePeca'])) {
+    
+}
+
+?>
--- views/cadastroPeca.php
+++ views/cadastroPeca.php
@@ -22,61 +22,61 @@
         </div>    
 
         <section class="section-content">
-            <form>
+            <form action="../models/Code_Peca.php" method="POST" class="ml-3 ml-md-3 ml-lg-3 col-12 col-lg-12">
                 <div class="row">
                     <div class="col-12" style="display: flex; margin-bottom: 30px; margin-top: 45px">
-                        <div class="col-6">
+                        <!-- <div class="col-6">
                             <label for="codpeca">Código Peça:</label>
                             <input type="text" name="username" class="form-control campoDigitarCadasServico">
-                        </div>                           
+                        </div>                            -->
                         <div class="col-6">
                             <label for="nomepeça">Nome Peça:</label>
-                            <input type="text" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="text" name="NomePeca" class="form-control campoDigitarCadasServico">
                         </div>                                        
                     </div>
 
                     <div class="col-12" style="display: flex; margin-bottom: 30px;">
                         <div class="col-6">
                             <label for="marca">Marca:</label>
-                            <input type="text" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="text" name="MarcaPeca" class="form-control campoDigitarCadasServico">
                         </div>
                         <div class="col-6">
                             <label for="quantidade">Quantidade:</label>
-                            <input type="text" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="text" name="QtdPeca" class="form-control campoDigitarCadasServico">
                         </div>                  
                     </div>
 
                     <div class="col-12" style="display: flex; margin-bottom: 30px;">
                         <div class="col-6">
                             <label for="cnpjfornecedor">CNPJ do Fornecedor:</label>
-                            <input type="text" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="text" name="CnpjForncedor" class="form-control campoDigitarCadasServico">
                         </div>
                         <div class="col-6">
                             <label for="nomefornecedor">Nome do Fornecedor:</label>
-                            <input type="text" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="text" name="NomeFornecedor" class="form-control campoDigitarCadasServico">
                         </div>                   
                     </div>
 
                     <div class="col-12" style="display: flex; margin-bottom: 30px;">
                         <div class="col-6">
                             <label for="telefone">Contato do Fornecedor:</label>
-                            <input type="tel" id="telefone" class="form-control campoDigitarCadasServico" required placeholder="(xx) xxxxx-xxxx">
+                            <input type="tel" name="TelFonecedor" class="form-control campoDigitarCadasServico" required placeholder="(xx) xxxxx-xxxx">
                         </div>
                         <div class="col-6">
                             <label for="datapedido">Data de Pedido:</label>
-                            <input type="date" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="date" name="DataPedido" class="form-control campoDigitarCadasServico">
                         </div>                   
                     </div>
 
                     <div class="col-12" style="display: flex; margin-bottom: 30px;">
                         <div class="col-6">
                             <label for="datarecebimento">Data Recebimento:</label>
-                            <input type="date" name="username" class="form-control campoDigitarCadasServico">
+                            <input type="date" name="DataRecebimento" class="form-control campoDigitarCadasServico">
                         </div>
                         <div class="col-6">
                             <label for="fotopeca">Adicione imagens da peça:</label>
                             <div class="input-group">
-                                <input type="file" class="form-control" style = "border: none;" accept="image/*">
+                                <input type="file" name="ImagemPeca" class="form-control" style = "border: none;" accept="image/*">
                                 <button class="btn btn-outline-secondary excluirFoto, border-0" type ="reset">X</button>
                             </div>
                         </div>                   
@@ -85,12 +85,12 @@
                     <div class="col-12" style="display: flex; margin-bottom: 20px;">
                         <div class="col-12">
                             <label for="username">Descrição:</label>
-                            <textarea class="campoObservacoesCadasServico" name="descricoes" rows="6" cols="50"></textarea>
+                            <textarea class="campoObservacoesCadasServico" name="DescricaoPeca" rows="6" cols="50"></textarea>
                         </div>                  
                     </div>  
 
                     <div class="col-12 text-right">
-                        <button type="submit" class="botaoOrdem">Cadastrar</button>
+                        <button type="submit" name="SavePeca" class="botaoOrdem">Cadastrar</button>
                     </div>
                     
                 </div>               
