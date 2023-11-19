327d48a91fef4a0f1963b2d6ee10f2995ec1b0ec
Állysson Ernandes <allyssonoliveira12345@gmail.com>
2023-11-18 21:30:37 -0300

    fix
    adiciona cor para dar destaque ao alert

--- utils/message.php
+++ utils/message.php
@@ -8,7 +8,7 @@
 ?>
     <link rel="stylesheet" href="../css/style.css">
     <div class=" d-flex justify-content-center col-12 pt-5">
-        <div class="alert fade show col-6" role="alert">
+        <div class="alert fade show col-12 alert-primary" role="alert">
             <div class="d-flex justify-content-center">
                 <?= $_SESSION['message']; ?>
             </div>
