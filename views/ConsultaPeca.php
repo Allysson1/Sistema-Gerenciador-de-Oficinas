c220154aad1c464ff25943eb4a8dd2c33c8f8789
AnaFurtonato <anafortunato44@gmail.com>
2023-11-23 22:38:11 -0300

    correcoesTelas

--- views/ConsultaPeca.php
+++ views/ConsultaPeca.php
@@ -11,7 +11,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/style.css" type="text/css">
+        <link rel="stylesheet" href="../css/style.css?v=<?php echo time(); ?>">
         <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -61,7 +61,7 @@
 
                 ?>
                 
-                <div class="col-sm-4 col-4">
+                <div class="col-sm-12 col-4">
                     <div class="cardPeca">
 
                         <div class="todosItensCard">
